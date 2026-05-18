9cbafc89d550fc7c74385b90af9e2381e1e6b907
ArcticLuma113 <aplumafreak500@gmail.com>
2026-05-18 16:15:43 -0400

    Add mr0k encryption

--- blktool.c
+++ blktool.c
@@ -14,6 +14,7 @@
 #include "blk.h"
 #include "mhy0.h"
 #include "mhy1.h"
+#include "mr0k.h"
 #include "ec2b.h"
 
 static void usage() {
@@ -342,8 +343,6 @@ static unsigned int mhy1_main(unsigned int argc, const char** argv) {
 	return -1;
 }
 
-extern int mr0k_decrypt(uint8_t* buf, size_t buf_size, unsigned int mode);
-
 static unsigned int mr0k_main(unsigned int argc, const char** argv) {
 	if (argc < 4) {
 		//mr0k_usage();
@@ -352,10 +351,6 @@ static unsigned int mr0k_main(unsigned int argc, const char** argv) {
 	const char* mode = argv[1];
 	const char* in_file = argv[2];
 	const char* out_file = argv[3];
-	const char* seed_file = NULL;
-	if (argc >= 4) {
-		seed_file = argv[4];
-	}
 	FILE* in_fp = fopen(in_file, "rb");
 	if (in_fp == NULL) {
 		fprintf(stderr, "input file open error: %s\n", strerror(errno));
@@ -366,31 +361,9 @@ static unsigned int mr0k_main(unsigned int argc, const char** argv) {
 		fprintf(stderr, "output file open error: %s\n", strerror(errno));
 		return -1;
 	}
-	FILE* seed_fp = NULL;
-	seed_t seed;
 	uint8_t* buf;
 	size_t bufSz;
 	if (strncasecmp(mode, "encrypt", 8) == 0) {
-#if 1
-		fprintf(stderr, "mr0k encryption is not supported yet\n");
-		return 100;
-#else
-		unsigned int hasSeed = 0;
-		if (seed_file != NULL) {
-			seed_fp = fopen(seed_file, "rb");
-		}
-		if (seed_fp != NULL) {
-#ifndef NDEBUG
-			memset(&seed, 0, sizeof(seed));
-#endif
-			fread(&seed, sizeof(seed), 1, seed_fp);
-			fclose(seed_fp);
-			fprintf(stderr, "Read from seed file %s\n", seed_file);
-			hasSeed = 1;
-		}
-		else {
-			getrandom(&seed, sizeof(seed) - sizeof(uint16_t), 0);
-		}
 		fseek(in_fp, 0, SEEK_END);
 		bufSz = ftell(in_fp);
 		fseek(in_fp, 0, SEEK_SET);
@@ -401,23 +374,13 @@ static unsigned int mr0k_main(unsigned int argc, const char** argv) {
 		}
 		fread(buf, bufSz, 1, in_fp);
 		fclose(in_fp);
-		if (!hasSeed) seed.mr0kSz = bufSz <= 2048 ? bufSz : 2048;
-		seed.mr0kSz = be16toh(seed.mr0kSz) & ~7;
-		seed.seed = be64toh(seed.seed);
 		fprintf(stderr, "Read from input file %s\n", in_file);
-		fprintf(stderr, "\t(key1 hex: %016llx%016llx key2 hex: %016llx%016llx seed size: %hu seed: 0x%016llx)\n", (unsigned long long) be64toh(((uint64_t*) seed.key)[0]), (unsigned long long) be64toh(((uint64_t*) seed.key)[1]), (unsigned long long) be64toh(((uint64_t*) seed.key)[2]), (unsigned long long) be64toh(((uint64_t*) seed.key)[3]), seed.mr0kSz, (unsigned long long) seed.seed);
-		encrypt_mr0k(buf, bufSz, seed.mr0kSz, seed.key, seed.seed);
-		hdr.magic = htobe32(0x626c6b00);
-		hdr.version = htole32(16);
-		memcpy(hdr.key1, seed.key, 32);
-		hdr.mr0kSz = htole16(seed.mr0kSz);
-		fwrite(&hdr, sizeof(hdr), 1, out_fp);
+		mr0k_encrypt(buf, bufSz, 1);
+		*(uint32_t*) buf = htobe32(0x6d72306b);
 		fwrite(buf, bufSz, 1, out_fp);
 		fclose(out_fp);
 		fprintf(stderr, "Wrote to output file %s\n", out_file);
-		fprintf(stderr, "\t(key1 hex: %016llx%016llx key2 hex: %016llx%016llx seed size: %hu)\n", (unsigned long long) be64toh(((uint64_t*) seed.key)[0]), (unsigned long long) be64toh(((uint64_t*) seed.key)[1]), (unsigned long long) be64toh(((uint64_t*) seed.key)[2]), (unsigned long long) be64toh(((uint64_t*) seed.key)[3]), seed.mr0kSz);
 		return 0;
-#endif
 	}
 	else if (strncasecmp(mode, "decrypt", 8) == 0) {
 		fseek(in_fp, 0, SEEK_END);
